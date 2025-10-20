ffbc47f391c0f5e959a15123d6a76639d001ce7d
mezunnk <edersonbarros@icloud.com>
2025-10-20 13:37:36 -0300

    vanish.

--- BrainFlow.Data/obj/Debug/net9.0/BrainFlow.Data.AssemblyInfo.cs
+++ BrainFlow.Data/obj/Debug/net9.0/BrainFlow.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrainFlow.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82bb8fe4cfcd7e951ff18dd09d93817a25334eeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40c623c5e013f70681a69bcbbf399584337c53fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrainFlow.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrainFlow.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
